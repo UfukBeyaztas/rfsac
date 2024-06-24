2116d29d0c657acbc43650800eb68573e5f55dbb
Ufuk Beyaztas <58028887+UfukBeyaztas@users.noreply.github.com>
2024-06-24 23:51:07 +0300

    Delete R/alpha_fun.R

--- R/alpha_fun.R
+++
@@ -1,7 +0,0 @@
-# alpha_fun is used to compute the weight matrix in the M-estimator
-alpha_fun <- function(z, c){
-  # z       : standardized residuals
-  # c       : tuning parameter
-  
-  ifelse(z == 0, 1, psi_fun(z, c) * z^{-1})
-}
