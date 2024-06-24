5949f4beab64a143bf3a4e0a4403dd76fd7a5518
Ufuk Beyaztas <58028887+UfukBeyaztas@users.noreply.github.com>
2024-06-24 23:50:30 +0300

    Delete R   directory

--- R  /hfun.R
+++
@@ -1,6 +0,0 @@
-# h tilde function
-hfun <- function(c){
-  # c       : tuning parameter
-  
-  2 * c^2 * (1 - pnorm(c)) - 2 * c * dnorm(c) - 1 + 2 * pnorm(c)
-}
--- R  /psi_fun.R
+++
@@ -1,7 +0,0 @@
-# Huber function
-psi_fun <- function(z, c){
-  # z       : standardized residuals
-  # c       : tuning parameter
-  
-  z * pmin(1, c / abs(z))
-}
